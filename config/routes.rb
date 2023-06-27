db01bbfc33898f40879683675cf5ecb386114f18
ballo1988 <rounderjards@gmail.com>
2023-06-26 21:17:45 -0500

    Corrected some errors
    co-authored-by: hugocruz1234 cruzhugo1997@gmail.com
    co-authored-by: sr-meh hector.m.rm@hotmail.com
    co-authored-by: JoseLopezSolis jlopez114@ucol.mx

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get '/events', to: "events#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/events', to: 'events#index'
 end
