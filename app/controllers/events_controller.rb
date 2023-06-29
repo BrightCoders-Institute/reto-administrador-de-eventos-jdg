8093c17f670e5cd8fd2408e79e1c95315d8ff500
hugocruz1234 <root@LAPTOP-TC1RTTI2.localdomain>
2023-06-28 21:08:04 -0600

    CRUD actions are finished sr-meh hector.m.rm@hotmail.com ballo1988 rounderjards@gmail.com JoseLopezSolis jlopez114@ucol

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,6 +20,27 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(events_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   def events_params
     params.require(:event).permit(:title, :description, :date, :location, :cost)
   end
