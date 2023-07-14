519f1615c16ea31bec54a433b669b0deb82afd00
ballo1988 <rounderjards@gmail.com>
2023-07-13 20:09:54 -0500

    Now export csv works with filter, so it only exports filtereded elements if desired
    co-authored-by: hugocruz1234 cruzhugo1997@gmail.com
    co-authored-by: sr-meh hector.m.rm@hotmail.com
    co-authored-by: JoseLopezSolis jlopez114@ucol.mx
    co-authored-by: jcardenas0 jcardenas0@ucol.mx

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,23 +3,18 @@ class Event < ApplicationRecord
   belongs_to :user
   validates :title, :date, :location, presence: true
 
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[cost created_at date description id location public title updated_at user_id specific_date]
+  end
 
   def self.to_csv
-    attributes = %w[id title description date location cost]
+    attributes = %w[title description date location cost]
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
-
       all.each do |event|
         csv << attributes.map { |attr| event.send(attr) }
       end
     end
   end
-  
-
-  def self.ransackable_attributes(auth_object = nil)
-    ["cost", "created_at", "date", "description", "id", "location", "public", "title", "updated_at", "user_id",'specific_date']
-  end
-
-
 end
